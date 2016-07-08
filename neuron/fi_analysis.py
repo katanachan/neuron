aa02e58a840cceefb47ec83c6b5fe5e6a8b5a3f4
katanachan <shwarya.unnikrishnan@gmail.com>
2016-07-09 04:44:10 +0530

    Class 1, Class 2 and Class 3 have been updated

--- neuron/fi_analysis.py
+++ neuron/fi_analysis.py
@@ -2,10 +2,12 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.optimize import curve_fit
 
-data = np.loadtxt('FIclass1.txt')
-np.insert(data,0,0)
+data = np.genfromtxt('FI3.txt')
+data2 = np.genfromtxt('class3.txt')
 x=data[:,0]
 y=data[:,1]
+t=data2[:,0]
+v=data2[:,1]
 z = np.polyfit(x, y, 4)
 f = np.poly1d(z)
 
@@ -13,9 +15,18 @@ f = np.poly1d(z)
 x_new = np.linspace(x[0], x[-1], 50)
 y_new = f(x_new)
 
+plt.figure()
+plt.subplot(211)
 plt.plot(x,y,'o', x_new, y_new)
 plt.xlim(xmin=0)
+plt.xlabel('current (in pA)')
+plt.ylabel('firing rate (in Hz')
 plt.ylim(ymin=0)
+plt.subplot(212)
+plt.plot(t,v,'r*',linestyle='-')
+plt.ylabel('voltage (in mV)')
+plt.xlabel('time step(in t)')
+plt.xlim(xmax=1)
 plt.show()
 #plt.plot(data[:,0],data[:,1],'r*',linestyle='-')
 #plt.show()
--- neuron/x86_64/mod_func.c
+++ neuron/x86_64/mod_func.c
@@ -3,6 +3,7 @@
 extern int nrnmpi_myid;
 extern int nrn_nobanner_;
 
+extern void _h_reg(void);
 extern void _kdrca1_reg(void);
 extern void _na3_reg(void);
 
@@ -10,10 +11,12 @@ void modl_reg(){
   if (!nrn_nobanner_) if (nrnmpi_myid < 1) {
     fprintf(stderr, "Additional mechanisms from files\n");
 
+    fprintf(stderr," h.mod");
     fprintf(stderr," kdrca1.mod");
     fprintf(stderr," na3.mod");
     fprintf(stderr, "\n");
   }
+  _h_reg();
   _kdrca1_reg();
   _na3_reg();
 }
