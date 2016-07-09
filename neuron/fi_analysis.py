dbde9e67f5e5d6d1e6d8f9b8f2fd1a16a40e399f
katanachan <shwarya.unnikrishnan@gmail.com>
2016-07-09 10:56:17 +0530

    Some corrections to the plotting

--- neuron/fi_analysis.py
+++ neuron/fi_analysis.py
@@ -2,12 +2,12 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.optimize import curve_fit
 
-data = np.genfromtxt('FI3.txt')
-data2 = np.genfromtxt('class3.txt')
+data = np.genfromtxt('FI1.txt')
+data2 = np.genfromtxt('class1.txt')
 x=data[:,0]
 y=data[:,1]
 t=data2[:,0]
-v=data2[:,1]
+v=data2[:,1]+65
 z = np.polyfit(x, y, 4)
 f = np.poly1d(z)
 
@@ -17,15 +17,16 @@ y_new = f(x_new)
 
 plt.figure()
 plt.subplot(211)
+plt.plot(x,y,'o')
 plt.plot(x,y,'o', x_new, y_new)
 plt.xlim(xmin=0)
 plt.xlabel('current (in pA)')
-plt.ylabel('firing rate (in Hz')
+plt.ylabel('firing rate (in mHz')
 plt.ylim(ymin=0)
 plt.subplot(212)
 plt.plot(t,v,'r*',linestyle='-')
 plt.ylabel('voltage (in mV)')
-plt.xlabel('time step(in t)')
+plt.xlabel('current step(in pa)')
 plt.xlim(xmax=1)
 plt.show()
 #plt.plot(data[:,0],data[:,1],'r*',linestyle='-')
